b06004e49d273fda97b6380b3c0ee56847365224
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-03-30 17:13:39 -0300

    boilerplate for the full stack app

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('#clickMe').addEventListener('click', makeReq)
+
+async function makeReq(){
+
+  const userName = document.querySelector("#userName").value;
+  //request to the server the fetch  
+  const res = await fetch(`/api?student=${userName}`)
+  const data = await res.json()
+
+  console.log(data);
+  document.querySelector("#personName").textContent = data.name
+  document.querySelector("#personStatus").textContent = data.status
+  document.querySelector("#personOccupation").textContent = data.currentOccupation
+}
